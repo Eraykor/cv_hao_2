e44fe1c6a19c79a2e6e17e0f692ab7cb39e027a7
Eraykor <contact.li.jinhao@gmail.com>
2016-03-29 11:12:07 +0200

    retouche images

---
+++ version.js
@@ -0,0 +1,7 @@
+function language() {
+    if (navigator.language == "fr") {
+        document.location.href="cv_fr/index.html"; 
+    } else {
+        document.location.href="cv_eng/index.html"; 
+    }
+}
